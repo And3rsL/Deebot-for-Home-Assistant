092d9a9fd3c4e999a94bb81927a0b66535e72fd8
andrea.liosi <andrea.liosi@gmail.com>
2021-01-26 13:32:54 +0100

    T8 Status fix

--- custom_components/deebot/__init__.py
+++ custom_components/deebot/__init__.py
@@ -15,7 +15,7 @@ from homeassistant.helpers import discovery
 from homeassistant.helpers.entity import Entity
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, EVENT_HOMEASSISTANT_STOP
 
-REQUIREMENTS = ['deebotozmo==1.7.5']
+REQUIREMENTS = ['deebotozmo==1.7.6']
 
 CONF_COUNTRY = "country"
 CONF_CONTINENT = "continent"
