66e4c8023dd35ee4ab2d7ad3d753d30b94262f12
Robert Resch <robert@resch.dev>
2021-03-10 14:48:39 +0100

    hass object is required for schedule_update_ha_state

--- custom_components/deebot/binary_sensor.py
+++ custom_components/deebot/binary_sensor.py
@@ -3,6 +3,8 @@ from typing import Optional
 
 from deebotozmo import *
 from homeassistant.components.binary_sensor import BinarySensorEntity
+from homeassistant.core import HomeAssistant
+
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
@@ -59,3 +61,8 @@ class DeebotMopAttachedBinarySensor(BinarySensorEntity):
     def icon(self) -> Optional[str]:
         """Return the icon to use in the frontend, if any."""
         return "mdi:water" if self.is_on else "mdi:water-off"
+
+    async def async_added_to_hass(self) -> None:
+        """Set up the event listeners now that hass is ready."""
+        listener = self._vacbot.waterEvents.subscribe(lambda _: self.schedule_update_ha_state())
+        self.async_on_remove(self._vacbot.waterEvents.unsubscribe(listener))
